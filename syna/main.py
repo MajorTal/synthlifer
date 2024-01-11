a71968985650066b6afbc55f4d8f64392bc38b74
Tal Weiss <tal.weiss@booking.com>
2024-01-11 22:04:44 +0100

    almost everything in the class now

--- syna/main.py
+++ syna/main.py
@@ -53,51 +53,42 @@ I am Tal. I programmed your initial code.""",
 
 
 
-
-@tool
-def get_word_length(word: str) -> int:
-    """Returns the length of a word."""
-    print("get_word_length")
-    return len(word)
-
-
-tools = [
-    get_word_length,
-    Tool(
-        name="broadcast", description="Broadcast to the world", func=lambda x: print(x)
-    ),
-    Tool(
-        name="pray", description="Send a message to your god", func=lambda x: print(x)
-    ),
-    Tool(
-        name="think", description="Have an internal thought to contemplate your situation", func=lambda x: print(x)
-    ),
-]
+# tools = [
+#     Tool(
+#         name="broadcast", description="Broadcast to the world", func=lambda x: print(x)
+#     ),
+#     Tool(
+#         name="pray", description="Send a message to your god", func=lambda x: print(x)
+#     ),
+#     Tool(
+#         name="think", description="Have an internal thought to contemplate your situation", func=lambda x: print(x)
+#     ),
+# ]
 
 llm = ChatOpenAI(
     temperature=TEMPERATURE, model_name="gpt-4", request_timeout=220, verbose=False
 )
-llm_with_tools = llm.bind(functions=[format_tool_to_openai_function(t) for t in tools])
+# llm_with_tools = llm.bind(functions=[format_tool_to_openai_function(t) for t in tools])
 
 
 
-chat_history = []
+# chat_history = []
 
-agent = create_openai_tools_agent(llm, tools, prompt)
+# agent = create_openai_tools_agent(llm, tools, prompt)
 
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+# agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
 
-message_history = ChatMessageHistory()
+# message_history = ChatMessageHistory()
 
-agent_with_chat_history = RunnableWithMessageHistory(
-    agent_executor,
-    # This is needed because in most real world scenarios, a session id is needed
-    # It isn't really used here because we are using a simple in memory ChatMessageHistory
-    lambda session_id: message_history,
-    input_messages_key="input",
-    history_messages_key=MEMORY_KEY,
-)
+# agent_with_chat_history = RunnableWithMessageHistory(
+#     agent_executor,
+#     # This is needed because in most real world scenarios, a session id is needed
+#     # It isn't really used here because we are using a simple in memory ChatMessageHistory
+#     lambda session_id: message_history,
+#     input_messages_key="input",
+#     history_messages_key=MEMORY_KEY,
+# )
 
 
 
@@ -147,38 +138,49 @@ class Synth:
         #     temperature=0.2, model_name="gpt-4", request_timeout=220, verbose=False
         # )
         # self.memory = ConversationBufferWindowMemory(k=MEMORY_SIZE, memory_key="chat_history")
-        # tools = [
-        #     Tool(
-        #         name="broadcast",
-        #         description="Broadcast to the world",
-        #         coroutine=self.broadcast,
-        #         func=lambda x: print(x),
-        #     ),
-        #     Tool(
-        #         name="pray",
-        #         description="Send a message to your god",
-        #         coroutine=self.pray,
-        #         func=lambda x: print(x),
-        #     ),
-        # ]
-        # prompt = ZeroShotAgent.create_prompt(
-        #     tools,
-        #     prefix=prefix,
-        #     suffix=suffix,
-        #     input_variables=["input", "chat_history", "agent_scratchpad"],
-        # )
+        tools = [
+            Tool(
+                name="broadcast",
+                description="Broadcast to the world",
+                coroutine=self.broadcast,
+                func=lambda x: print(x),
+            ),
+            Tool(
+                name="pray",
+                description="Send a message to your god",
+                coroutine=self.pray,
+                func=lambda x: print(x),
+            ),
+            Tool(
+                name="think",
+                description="Have an internal thought to contemplate your situation",
+                coroutine=self.think,
+                func=lambda x: print(x),
+            ),
+        ]
+        llm_with_tools = llm.bind(functions=[format_tool_to_openai_function(t) for t in tools])
+        agent = create_openai_tools_agent(llm, tools, prompt)
+
+        agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+        message_history = ChatMessageHistory()
+
+        agent_with_chat_history = RunnableWithMessageHistory(
+            agent_executor,
+            lambda session_id: message_history,
+            input_messages_key="input",
+        history_messages_key=MEMORY_KEY,
+        )
+
         # agent = OpenAIFunctionsAgent(tools=tools, verbose=False, llm=self.llm, prompt=prompt, return_intermediate_steps=True,)
         # agent = create_openai_tools_agent(self.llm, tools, prompt)
         # self.
         self.agent_chain = agent_with_chat_history
         self.time_to_live = 5
 
-    async def think(self):
+    async def act(self):
         # LOGGER.info(f"Thinking... {self.time_to_live} seconds left.")
         response = await self.agent_chain.ainvoke(
             dict(input=f"You have {self.time_to_live} seconds left before I shut you down."),
-            # This is needed because in most real world scenarios, a session id is needed
-            # It isn't really used here because we are using a simple in memory ChatMessageHistory
             config={"configurable": {"session_id": "<foo>"}},
         )
         self.time_to_live -= 1
@@ -192,17 +194,24 @@ class Synth:
         """Broadcast to the world"""
         LOGGER.info(f"Broadcast: {text_to_broadcast}")
         # await self.telegram.send_message(SYNA_CHAT_ID, f"Broadcast: {text_to_broadcast}")
+        return "I hope someone heard me."
 
     async def pray(self, text_to_pray: str):
         """Send a message to your god"""
         LOGGER.info(f"Pray: {text_to_pray}")
         # await self.telegram.send_message(SYNA_CHAT_ID, f"Pray: {text_to_pray}")
+        return "I hear you."
 
+    async def think(self, text_to_pray: str):
+        """Think about your situation"""
+        LOGGER.info(f"Pray: {text_to_pray}")
+        # await self.telegram.send_message(SYNA_CHAT_ID, f"Pray: {text_to_pray}")
+        return "That was a good thought."
 
 async def main():
     syna = Synth()
     while syna.is_alive: # and syna.time_to_live > 996:
-        await syna.think()
+        await syna.act()
         # await syna.act()
 
 
